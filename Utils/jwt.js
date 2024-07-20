fcec016eea64fed8ff5abe64724879d39c9ca09c
ManishK1832 <mk1169852@gmail.com>
2024-07-20 14:59:58 +0530

    changes in api

---
+++ Utils/jwt.js
@@ -0,0 +1,52 @@
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import User from '../Models/user.js';
+import { redis } from "./redis.js";
+dotenv.config();
+
+export const sendToken = (user, statusCode, res) => {
+
+    
+    const accessToken = user.signAccessToken();
+    const refreshToken = user.signRefreshToken();
+
+    //upload session to redis
+    redis.set(user.userId,JSON.stringify(user))
+
+
+
+    //parse environment variable to integrate fall back values
+    const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || '5m');
+    const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || '7d');
+
+
+    //we have to add secure :true in production
+    const accessTokenOptions ={
+        expires: new Date(Date.now() + accessTokenExpire),
+        maxAge: accessTokenExpire,
+        httpOnly: true,
+        sameSite: 'lax'
+        //secure: true
+    }
+        //we have to add secure :true in production
+
+    const refreshTokenOptions ={
+        expires: new Date(Date.now() + refreshTokenExpire),
+        maxAge: refreshTokenExpire,
+        httpOnly: true,
+        sameSite: 'lax'
+        //secure: true
+    }
+
+    res.cookie('access_token', accessToken, accessTokenOptions);
+    res.cookie('refresh_token', refreshToken, refreshTokenOptions);
+
+
+
+    res.status(statusCode).json({
+        success: true,
+        user,
+        accessToken,
+        refreshToken
+    })
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 const userRouter = express.Router();
 
-
-
-import { registrationUser,activateUser,setPassword } from "../Controllers/user.controller.js";
+import { registrationUser,activateUser,setPassword,loginUser,logoutUser } from "../Controllers/user.controller.js";
+import { isAuthenticated } from "../Middlewares/auth.js";
 
 
 userRouter.post("/registration", registrationUser);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/set-password", setPassword);
-
+userRouter.post("/login", loginUser);
+userRouter.get("/logout", isAuthenticated,logoutUser);
 
 export default userRouter
