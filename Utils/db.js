8c9f95c253a8bcfae33903f4c6224a5e38c489bb
ManishK1832 <mk1169852@gmail.com>
2024-08-31 12:38:12 +0530

    changes done

--- Utils/db.js
+++ Utils/db.js
@@ -15,7 +15,7 @@ const connectDB = () => {
 
             dialectOptions: {
                 ssl: {
-                    require: true,
+                    require: false,
                     rejectUnauthorized: false 
                 }
             }
