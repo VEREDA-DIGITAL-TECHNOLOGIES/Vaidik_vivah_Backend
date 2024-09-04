25e47e855470df26c340ca022368d39e7fd922da
manish kumar <mk1169852@gmail.com>
2024-09-04 18:05:18 +0530

    changes in jwt

--- Utils/jwt.js
+++ Utils/jwt.js
@@ -25,7 +25,7 @@ export const sendToken = (user, statusCode, res,message) => {
         expires: new Date(Date.now() + accessTokenExpire),
         maxAge: accessTokenExpire,
         httpOnly: true,
-        sameSite: 'none',
+        sameSite: 'lax',
         secure: true
     }
         //we have to add secure :true in production
@@ -34,7 +34,7 @@ export const sendToken = (user, statusCode, res,message) => {
         expires: new Date(Date.now() + refreshTokenExpire),
         maxAge: refreshTokenExpire,
         httpOnly: true,
-        sameSite: 'none',
+        sameSite: 'lax',
         secure: true
     }
 
