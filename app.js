afd5bbda891c49f1d455049d0524abf813b5f229
manish kumar <mk1169852@gmail.com>
2024-10-09 16:11:24 +0530

    changes in app js set cors

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: ["https://kashishmashala.com"], 
+    origin: ["https://wedlock.au", "http://localhost:3000"], 
     credentials: true,  
   }));
   
