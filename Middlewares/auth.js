77558b66a4d252c3c9f470f8142835347a92ea46
manish kumar <mk1169852@gmail.com>
2025-02-11 16:58:48 +0530

    changes in isAuthenticated

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -6,8 +6,9 @@ import jwt from "jsonwebtoken";
 //authenticated user
 
 export const isAuthenticated = catchAsyncError(async (req, res, next) => {
-    const accessToken = req.cookies.access_token || req.header("Authorization");
+    const accessToken = req.cookies?.access_token || req.header("Authorization");
    
+    console.log(accessToken);
 
     if (!accessToken) {
         return next(new errorhandler("Please login to Find perfect matches", 400));
