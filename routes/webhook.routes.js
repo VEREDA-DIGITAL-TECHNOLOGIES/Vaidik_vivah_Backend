e139d19538f52b3c2196a01faff49131721e676a
manish kumar <mk1169852@gmail.com>
2025-02-11 19:16:08 +0530

    changs done

--- routes/webhook.routes.js
+++ routes/webhook.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { handlePaymentSuccess } from "../Controllers/webhook.controller.js";
 
+const StipeApp = express.Router();
+StipeApp.use(express.raw({ type: '*/*' }));
+
+
 const webhookRouter = express.Router();
 
-// Stripe requires raw body for signature verification
-webhookRouter.post('/webhook', express.raw({ type: 'application/json' }), handlePaymentSuccess);
 
-export default webhookRouter;
+webhookRouter.post('/webhook', StipeApp, handlePaymentSuccess);
+
+export default webhookRouter 
