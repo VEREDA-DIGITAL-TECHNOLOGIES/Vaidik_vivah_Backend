6590fd559773582fd698c621ea90291ced81004a
Abhikhush28 <sahuabhikhush@gmail.com>
2025-06-27 10:36:26 +0530

    ip have added

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ const startServer = async () => {
         //     console.log(`Server is running on port ${PORT}`);
         // });
 
-        server.listen(PORT, () => {
+        server.listen(PORT, '0.0.0.0', () => {
             console.log(`Server is running on port ${PORT}`);
         });
         
