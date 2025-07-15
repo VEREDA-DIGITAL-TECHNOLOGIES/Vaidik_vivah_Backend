ff799c877765d0b4dc1e37a1dab0fa2b742a3daf
Rahul Kushwaha <rahul.kushwaha9140@gmail.com>
2025-07-15 18:17:02 +0530

    cors fix

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3001', "https://recommend.vaidikvivah.in", 'http://localhost:8000', "https://vaidikvivah.in","https://www.vedvivah.in", ],
+  origin: ['http://localhost:5173', 'http://localhost:3001', "https://recommend.vaidikvivah.in", 'http://localhost:8000', "https://vaidikvivah.in","https://www.vedvivah.com", ],
   credentials: true,
 }));
 
