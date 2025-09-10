331d4fbbf4f609f6e5643ae52ed0b0ab31d233ef
Work.Abhishek <work.abhishek031@gmail.com>
2025-09-10 09:37:34 +0530

    Update plan.routes.js

--- routes/plan.routes.js
+++ routes/plan.routes.js
@@ -2,14 +2,14 @@ import express from "express";
 import { isAuthenticated } from "../Middlewares/auth.js";
 import { createPlan,getAllPlans,deletePlan,getPlanById,updatePlan } from "../Controllers/plan.controller.js";
 
-
+import { isAdminAuthenticated } from "../Middlewares/admin/isAdminAuthenticated.js";
 const planRouter = express.Router();
 
-planRouter.post('/createPlan',isAuthenticated,createPlan);
+planRouter.post('/createPlan',isAdminAuthenticated,createPlan);
 planRouter.get('/getAllPlans',getAllPlans);
-planRouter.delete('/deletePlan',isAuthenticated,deletePlan)
+planRouter.delete('/deletePlan',isAdminAuthenticated,deletePlan)
 
 planRouter.post("/getPlan", getPlanById);          // body { planId }
-planRouter.put("/updatePlan", updatePlan);
+planRouter.put("/updatePlan",isAdminAuthenticated, updatePlan);
  
 export default planRouter
